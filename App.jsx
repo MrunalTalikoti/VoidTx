91577158091eedc18d708d08afe9032f001e6d29
MrunalTalikoti <mrunaltalikoti2@gmail.com>
2025-12-18 10:56:36 +0530

    Monad_hackathon

---
+++ App.jsx
@@ -0,0 +1,13 @@
+/**
+ * Main App Entry Point - React
+ * This is the root component that renders VoidTxApp
+ */
+
+import React from 'react';
+import VoidTxApp from './frontend-utils/VoidTxApp';
+
+function App() {
+  return <VoidTxApp />;
+}
+
+export default App;
